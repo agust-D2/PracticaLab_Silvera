8b9a496b2841ca9d7b6f73667e69d6c412b006bf
Gomez Elias <elias.f.g.61@gmail.com>
2020-07-09 18:46:10 -0500

    LISTO

--- preg03.py
+++ preg03.py
@@ -1,1 +1,12 @@
 #Autor: Gomez Flores Elias
+
+nombre=input("ingrese nombre: ")
+juntar = nombre.split(' ')
+
+finnombre=""
+for n in juntar:
+    finnombre=finnombre+n
+
+
+tamaño=len(finnombre)
+print(tamaño)
