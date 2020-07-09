2169bfaca5a228b925d48e742d82865bb7a13e76
Orlando <orlando97rm@gmail.com>
2020-07-09 18:12:32 -0500

    Pregunta 02 - LISTO

--- preg02.py
+++ preg02.py
@@ -1,2 +1,12 @@
 # Autor: Jose Orlando Ramos Machuca
 
+def correoUntels(correo):
+    correo = correo.lower()
+    
+    if "@untels.edu.pe" in correo:
+        return "Si es correo de untels."
+    else:
+        return "No es correo de la untels."
+    
+correo = input("Ingrese un correo: ")
+print(correoUntels(correo))
